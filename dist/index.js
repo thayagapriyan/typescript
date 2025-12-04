6095478bf25763c6955a6d065ec561ee5616a0f2
thayagapriyan <rdsthayagan@gmail.com>
2025-12-03 23:21:29 -0600

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// src/index.ts
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+const userName = "TypeScript User";
+greet(userName);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// src/index.ts
+function greet(name: string): void {
+    console.log(`Hello, ${name}!`);
+}
+
+const userName: string = "TypeScript User";
+greet(userName);
